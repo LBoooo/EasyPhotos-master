ab5726e524f88ca175e079f6c80ea13ce09a63f3
Hinacle - <workerlibai@163.com>
2024-01-16 09:29:39 +0800

    no message

--- easyPhotos/src/main/java/com/huantansheng/easyphotos/ui/EasyPhotosActivity.java
+++ easyPhotos/src/main/java/com/huantansheng/easyphotos/ui/EasyPhotosActivity.java
@@ -213,17 +213,17 @@ public class EasyPhotosActivity extends AppCompatActivity implements AlbumItemsA
 
 
     protected String[] getNeedPermissions() {
-        return new String[]{
-                Manifest.permission.READ_MEDIA_IMAGES,
-                Manifest.permission.READ_MEDIA_VIDEO
-        };
-        /*
+//        return new String[]{
+//                Manifest.permission.READ_MEDIA_IMAGES,
+//                Manifest.permission.READ_MEDIA_VIDEO
+//        };
+
         if (Setting.isShowCamera) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
                 return new String[]{
                         Manifest.permission.CAMERA,
                         Manifest.permission.READ_MEDIA_IMAGES,
-                        Manifest.permission.READ_MEDIA_AUDIO,
+//                        Manifest.permission.READ_MEDIA_AUDIO,
                         Manifest.permission.READ_MEDIA_VIDEO
                 };
             }
@@ -239,7 +239,7 @@ public class EasyPhotosActivity extends AppCompatActivity implements AlbumItemsA
                 return new String[]{
                         Manifest.permission.CAMERA,
                         Manifest.permission.READ_MEDIA_IMAGES,
-                        Manifest.permission.READ_MEDIA_AUDIO,
+//                        Manifest.permission.READ_MEDIA_AUDIO,
                         Manifest.permission.READ_MEDIA_VIDEO
                 };
             }
@@ -251,7 +251,7 @@ public class EasyPhotosActivity extends AppCompatActivity implements AlbumItemsA
             return new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
         }
 
-         */
+
     }
 
 
